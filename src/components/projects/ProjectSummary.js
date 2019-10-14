52d1ed784ab905f088b37f2c58d6a395db6b8c74
Chinedu Emesue <de.magnificent1@gmail.com>
2019-10-14 08:42:01 +0100

    Started redux implementation

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
-
-const ProjectSummary = () =>{
+const ProjectSummary = ({project}) =>{
     return(
         <div className="card z-depth-0 project-summary">
             <div className="card-content grey-text text-darken-3">
-                <span className="card-title">Learn React</span>
+                <span className="card-title">{project.title}</span>
                 <p>Posted by Nedum</p>
                 <p className="grey-text">11th October, 3:44pm</p>
             </div>
---
+++ src/store/reducers/projectReducer.js
@@ -0,0 +1,13 @@
+const initState = {
+    projects: [
+        {id: 1, title: 'Our husband has gone mad again', content: 'The report reaching us confirms the story!'},
+        {id: 2, title: 'The gods are not to blame', content: 'Please dont blame the gods they are innocent'},
+        {id: 3, title: 'There was a country', content: 'Truth be told there was country'}
+    ]
+}
+
+const projectReducer = (state = initState, action) =>{
+    return state
+}
+
+export default projectReducer;
